9bb19276a34d5792241c3f7ebcf73ffaa95620a4
Yamanaka Tomohiro <yamanaka@un-t.com>
2025-06-06 16:04:22 +0900

    プロジェクト名の整理

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,4 +1,4 @@
-export const projectName = 'Astro With Constraints';
+export const projectName = 'Astro With Constraints2';
 export const projectDescription = '制約が強いプロジェクト向けAstro開発環境';
 export const projectUrl = 'https://example.com';
 export const projectDirectory = '/'; // ex: '/',  '/sample/'
