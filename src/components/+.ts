a3c407c575642fa70f5017841715d498bdbcf1b1
Yamanaka Tomohiro <yamanaka@un-t.com>
2025-11-14 12:00:34 +0900

    カルーセルライブラリをemblaに変更

--- src/components/+.ts
+++ src/components/+.ts
@@ -1,4 +1,6 @@
 import './Accordion/Accordion.ts';
 import './Carousel/Carousel.ts';
+import './Carousel/CarouselPagination.ts';
+import './Carousel/CrouselControls.ts';
 import './Modal/Modal.ts';
 import './Navigation/Navigation.ts';
--- src/components/Carousel/Carousel.ts
+++ src/components/Carousel/Carousel.ts
@@ -1,74 +1,23 @@
-import { matchMediaController } from '@/scripts/develop/matchMediaController';
-import { register, type SwiperContainer } from 'swiper/element/bundle';
+import EmblaCarousel from 'embla-carousel';
+import { addDotBtnsAndClickHandlers } from './CarouselPagination';
+import { addPrevNextBtnsClickHandlers } from './CrouselControls';
 
-register();
+const emblaNode = document.querySelector('.embla');
+const options: OptionsType = { containScroll: false };
+const dotsNode = emblaNode?.querySelector('.embla__dots');
+const prevBtnNode = emblaNode?.querySelector('.embla__button--prev');
+const nextBtnNode = emblaNode?.querySelector('.embla__button--next');
 
-const breakpoints = {
-  md: '48em',
-  lg: '75em',
-};
+const emblaApi = EmblaCarousel(emblaNode as unknown as HTMLElement, options);
+// console.log(emblaApi.slideNodes()); // Access API
 
-const carouselElements = Array.from(document.querySelectorAll('.js-carousel'));
-carouselElements.forEach((carousel) => {
-  const swiperElement: SwiperContainer | null = carousel.querySelector('[data-role="swiper"]');
-  const prevButton: HTMLButtonElement | null = carousel.querySelector('[data-role="prev"]');
-  const nextButton: HTMLButtonElement | null = carousel.querySelector('[data-role="next"]');
+const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode as HTMLElement);
 
-  if (!swiperElement) return;
+const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+  emblaApi,
+  prevBtnNode as HTMLElement,
+  nextBtnNode as HTMLElement,
+);
 
-  swiperElement.initialize();
-
-  const goNext = () => {
-    if (swiperElement) swiperElement.swiper.slideNext();
-  };
-
-  const goPrev = () => {
-    if (swiperElement) swiperElement.swiper.slidePrev();
-  };
-
-  nextButton?.addEventListener('click', goNext);
-  prevButton?.addEventListener('click', goPrev);
-
-  const changeButtonState = (element: SwiperContainer) => {
-    if (element.swiper.isEnd) {
-      nextButton?.setAttribute('disabled', 'true');
-    } else {
-      nextButton?.removeAttribute('disabled');
-    }
-    if (element.swiper.isBeginning) {
-      prevButton?.setAttribute('disabled', 'true');
-    } else {
-      prevButton?.removeAttribute('disabled');
-    }
-  };
-
-  swiperElement.swiper.on('slideChange', () => {
-    changeButtonState(swiperElement);
-  });
-  changeButtonState(swiperElement);
-
-  matchMediaController().init({
-    condition: `(width <= ${breakpoints.md})`,
-    callback: (match) => {
-      if (match) {
-        if (swiperElement) swiperElement.slidesPerView = 1;
-      }
-    },
-  });
-  matchMediaController().init({
-    condition: `(${breakpoints.md} < width <= ${breakpoints.lg})`,
-    callback: (match) => {
-      if (match) {
-        if (swiperElement) swiperElement.slidesPerView = 2;
-      }
-    },
-  });
-  matchMediaController().init({
-    condition: `(width > ${breakpoints.lg})`,
-    callback: (match) => {
-      if (match) {
-        if (swiperElement) swiperElement.slidesPerView = 3;
-      }
-    },
-  });
-});
+emblaApi.on('destroy', removeDotBtnsAndClickHandlers);
+emblaApi.on('destroy', removePrevNextBtnsClickHandlers);
