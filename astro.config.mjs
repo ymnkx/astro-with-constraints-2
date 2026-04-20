84959221589b72a46a6fe3d88cfc51aec376519b
Yamanaka Tomohiro <yamanaka@un-t.com>
2026-04-20 16:58:56 +0900

    Astro6にバージョンアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,26 +29,35 @@ export default defineConfig({
         },
       },
     },
+    environments: {
+      client: {
+        build: {
+          rollupOptions: {
+            output: {
+              entryFileNames: (info) => {
+                let fileName = 'script';
+                if (info.moduleIds) {
+                  const scriptsModule = info.moduleIds.find((id) => id.includes('/src/scripts/') && id.endsWith('.ts'));
+                  if (scriptsModule) {
+                    const match = scriptsModule.match(/\/src\/scripts\/([^/]+)\.ts$/);
+                    if (match) {
+                      fileName = match[1];
+                    }
+                  }
+                }
+                return `${assetsDir}/js/${fileName}.js`;
+              },
+            },
+          },
+        },
+      },
+    },
     build: {
       assetsInlineLimit: 0,
       rollupOptions: {
         output: {
-          entryFileNames: (info) => {
-            let fileName = 'script';
-            if (info.moduleIds) {
-              const scriptsModule = info.moduleIds.find((id) => id.includes('/src/scripts/') && id.endsWith('.ts'));
-              if (scriptsModule) {
-                const match = scriptsModule.match(/\/src\/scripts\/([^/]+)\.ts$/);
-                if (match) {
-                  fileName = match[1];
-                }
-              }
-            }
-            return `${assetsDir}/js/${fileName}.js`;
-          },
           assetFileNames: (info) => {
             const fileName = [...String(info.source).matchAll(/--output-file-name:\s*([^}]+)/g)][0]?.[1] || 'style';
-
             return info.names[0].endsWith('.css')
               ? `${assetsDir}/css/${fileName}.css`
               : info.names[0].endsWith('.js')
